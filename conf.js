28e04ee433391c738b8409ae03b0c23f7ca4ce32
elnarmirzayev <elnar.mirzayev@karsun-llc.com>
2018-05-05 12:45:07 -0400

    Implemented initial config

--- conf.js
+++ conf.js
@@ -1,3 +1,15 @@
-describe('Test', () => {
-    
-});
+exports.config = {
+    // The address of a running selenium server. 
+        seleniumAddress: 'http://localhost:4444/wd/hub', 
+   //Capabilities to be passed to the webdriver instance. 
+        capabilities: {
+             'browserName': 'chrome'    }, 
+    //Spec patterns are relative to the current working directly when protractor is called. 
+         specs: ['example_spec.js'],     
+    //framework to be used:
+	framework: 'jasmine2',
+    // Options to be passed to Jasmine-node. 
+    jasmineNodeOpts: {
+        showColors: true, 
+        defaultTimeoutInterval: 30000    
+}};
